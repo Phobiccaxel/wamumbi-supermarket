01389dbdf3bca87fc0014efeff65e50603d06258
Phobiccaxel <amolakech157@gmail.com>
2026-07-01 01:26:04 +0300

    Update Firebase configuration with new project details

--- firebase.js
+++ firebase.js
@@ -1,9 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "...",
-  authDomain: "...",
-  projectId: "...",
-  storageBucket: "...",
-  messagingSenderId: "...",
-  appId: "...",
-  measurementId: "..."
+  apiKey: "AIzaSyD0GvhWPCF4t3J5aDKky39D_kcc7hMpLWc",
+  authDomain: "wamumbi-supermarket.firebaseapp.com",
+  projectId: "wamumbi-supermarket",
+  storageBucket: "wamumbi-supermarket.firebasestorage.app",
+  messagingSenderId: "139514973658",
+  appId: "1:139514973658:web:1aca5c20e89da5240284e3",
+  measurementId: "G-CEBRJC1F78"
 };
