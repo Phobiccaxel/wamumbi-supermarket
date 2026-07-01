7c59d1a83d2a0177e59f1ce26084bfc012c1adf8
Phobiccaxel <amolakech157@gmail.com>
2026-07-01 10:54:07 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,23 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-analytics.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
 
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "wamumbi-supermarket.firebaseapp.com",
+  projectId: "wamumbi-supermarket",
+  storageBucket: "wamumbi-supermarket.firebasestorage.app",
+  messagingSenderId: "139514973658",
+  appId: "1:139514973658:web:1aca5c20e89da5240284e3",
+  measurementId: "G-CEBRJC1F78"
+};
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyD0GvhWPCF4t3J5aDKky39D_kcc7hMpLWc",
-    authDomain: "wamumbi-supermarket.firebaseapp.com",
-    projectId: "wamumbi-supermarket",
-    storageBucket: "wamumbi-supermarket.firebasestorage.app",
-    messagingSenderId: "139514973658",
-    appId: "1:139514973658:web:1aca5c20e89da5240284e3",
-    measurementId: "G-CEBRJC1F78"
-  };
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-  export { app };
+export { app, db };
